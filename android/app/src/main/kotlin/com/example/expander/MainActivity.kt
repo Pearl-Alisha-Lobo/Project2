3b033eac426a529585df878ae1db4b77550e6b4c
LUX\Ext03549 <pearl.alisha.lobo@signify.com>
2021-02-03 14:51:28 +0530

    Expander

---
+++ android/app/src/main/kotlin/com/example/expander/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.expander
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,58 @@
+// import 'package:flutter/material.dart';
+// import 'expander_list_panel.dart';
+// class Home extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'Flutter Expansion View',
+//         ),
+//         centerTitle: true,
+//         elevation: 0.0,
+//       ),
+//       body: Padding(
+//         padding: const EdgeInsets.symmetric(horizontal:30.0),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Row(
+//               children: <Widget>[
+//                 Expanded(
+//                   child: RaisedButton(
+//                     onPressed: (){
+//                       // Navigator.push(
+//                       //     context,
+//                       //     MaterialPageRoute(
+//                       //         builder: (BuildContext          context)=>Expansiontile()));
+//                     },
+//                     child: Text(
+//                         'ExpansionTile'
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//             Row(
+//               children: <Widget>[
+//                 Expanded(
+//                   child: RaisedButton(
+//                     onPressed: (){
+//                       Navigator.push(
+//                           context,
+//                           MaterialPageRoute(
+//                               builder: (BuildContext context)=>Expansionpanel()));
+//                     },
+//                     child: Text(
+//                         'ExpansionPanel'
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
